940aed65ad0a726acf6c8080f571ceda5c1f029b
abhinav chaubey <149956986+abhinavkumar0811@users.noreply.github.com>
2026-08-04 11:08:04 +0530

    Time: 2 ms (30.96%), Space: 60.5 MB (80.66%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -4,6 +4,20 @@
  */
 var reverseString = function(s) {
     
-    s.reverse()
-    
+    // s.reverse()
+
+    let left = 0;
+    let right = s.length-1;
+
+    while(left<right){
+
+        let temp  = s[left]
+        s[left] = s[right]
+        s[right] = temp
+
+        left++
+        right--
+    }
+
+
 };
