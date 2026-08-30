3670140bf04dd6e39a2f20077d1a7cb64e81a738
abhinav chaubey <149956986+abhinavkumar0811@users.noreply.github.com>
2026-08-30 10:10:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.py
@@ -0,0 +1,17 @@
+class Solution:
+	def countOddEven(self, arr):
+		#Code here
+		
+		even = 0
+		odd = 0
+		
+		for i in arr:
+		    
+		    if i%2==0:
+		        even +=1
+		    else:
+		        odd +=1
+
+
+        return [odd, even]
+		
